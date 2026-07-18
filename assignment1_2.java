42692b4af0f91a4c75aefb18ea25f9bbd7fa71fe
Arush440 <arushmittal440@gmail.com>
2026-07-18 12:35:29 +0530

    Add multiplication table program in assignment1_2.java

---
+++ assignment1_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class assignment1_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<11;j++){
+                System.out.println(i+ " * "+j+" = "+(i*j) );
+            }
+        }
+        sc.close();
+    }
+
+    
+}
+
